56e4d98ff5ea80c725931f50168cce172d05e11d
NicoFelixA <124410869+NicoFelixA@users.noreply.github.com>
2023-09-11 18:47:55 -0700

    Add files via upload

--- parcial1/pares.js
+++ parcial1/pares.js
@@ -1,7 +1,48 @@
 $(document).ready(function(){
-    var contador=0;
+    var carta1="";
+    var par=false;
+    var carta2="";
+    var total_pares=0;
     $('img').click(function(e){
+        var estado= $(this).attr('data-estado')
         var nombre_imagen= $(this).attr('data-id')
-        $(this).attr('src', 'fotos-pares/' + nombre_imagen);
+        if(estado=="0"){
+            if(carta1!= ""&& carta2!= ""){
+                console.log("Las cartas son diferentes")
+                if(carta1.attr('data-id')!=carta2.attr('data-id')){
+                    console.log("Tapa cartas diferentes")
+                    carta1.attr('src','fotos-pares/tapar.jpg')
+                    carta2.attr('src','fotos-pares/tapar.jpg')
+                }else{
+                    console.log("Las cartas son pares")
+                    carta1.attr('data-estado','1')
+                    carta2.attr('data-estado','1')
+                    par=false
+                }
+                carta1=""
+                carta2=""
+            }
+        }
+        if(estado=="0"){
+            console.log("Estado de la carta: " + estado)
+            if(carta1==""){
+                carta1=$(this);
+                carta1.attr('src','fotos-pares/' + nombre_imagen)
+                console.log("Se asigno carta #1");
+            }else{
+                carta2=$(this);
+                carta2.attr('src','fotos-pares/' + nombre_imagen)
+                console.log("Se asigno carta #2");
+            }
+        }
+        if(carta1 !="" && carta2!=""){
+             if(carta1.attr('data-id')==carta2.attr('data-id')){
+                par=true
+                total_pares++
+                $("#total_pares").html(total_pares)
+                carta1="";
+                carta2="";
+             }
+        }
     });
 });
