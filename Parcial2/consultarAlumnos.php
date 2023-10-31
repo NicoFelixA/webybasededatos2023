80b61a4f67f63e3f2a9d9e2a915e81f6231095b1
NicoFelixA <124410869+NicoFelixA@users.noreply.github.com>
2023-10-30 19:27:22 -0700

    Add files via upload

--- Parcial2/consultarAlumnos.php
+++ Parcial2/consultarAlumnos.php
@@ -3,40 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consultar alumnos</title>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <script src="code.jquery.com_jquery-3.7.1.js"></script>
+    <script src="jquery-3.7.1.js"></script>
 </head>
 <body>
-    <?php 
-        include 'menu.php';
-        include 'conexion.php';
-        $sql="SELECT * FROM alumnos";
-        $datos=$conexion->query($sql);
+    <?php
+    include 'menu.php';
+    include 'conexion.php';
+
+    $sql = "SELECT * FROM alumnos";
+    $datos = $conexion->query($sql);
+
     ?>
-    
-    <div class="container">
+
+    <div class="contariner">
         <div class="row">
             <div class="col-12">
                 <?php if($datos->num_rows>0){ ?>
                     <table class="table table-hover">
-                        <thead>
+                    <thead>
+                        <tr>
+                            <tr>ID</tr>
+                            <tr>Nombre</tr>
+                            <tr>Numero de control</tr>
+                            <tr>Semestres</tr>
+                            <tr>Edad</tr>
+                            <tr>Turno</tr>
+                            <tr>Sexo</tr>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while($registro = $datos->fetch_assoc()){ ?>
                             <tr>
-                                <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Número de control</th>
-                                <th>Semestre</th>
-                                <th>Edad</th>
-                                <th>Turno</th>
-                                <th>Sexo</th>
+                                <td><?php echo $registro["id"]; ?></td>
+                                <td><?php echo $registro["nombre"]; ?></td>
+                                <td><?php echo $registro["numero_control"]; ?></td>
+                                <td><?php echo $registro["semestre"]; ?></td>
+                                <td><?php echo $registro["edad"]; ?></td>
+                                <td><?php echo $registro["turno"]; ?></td>
+                                <td><?php echo $registro["sexo"]; ?></td>
+                                <td>
+                                    <a href="editarAlumno.php?id=<?php echo $registro["id"];?>" class="btn btn-xs btn-primary">Editar</a>
+                                    <a href="eliminarAlumno.php<?php echo $registro["id"];?>" class="btn btn-xs btn-danger">Eliminar</a>
+                                </td>
                             </tr>
-                        </thead>
+
+                        <?php } ?>
+
+                    </tbody>
                     </table>
-                <?php} else{ ?>
-                    <h2>No existe datos de alumnos en la base de datos</h2>
-                    <?php} ?>
+                </div>
+                <?php } else { ?>
+                        <h2 class="m-4">No existe datos de alumnos en la base de datos</h2>
+                    <?php } ?>
+                
+                
             </div>
         </div>
     </div>
+    
 </body>
 </html>
