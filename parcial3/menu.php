b244da1f7064175ce93cdfa1698d0f1ceb2b8ff2
NicoFelixA <124410869+NicoFelixA@users.noreply.github.com>
2023-11-13 19:18:29 -0700

    Add files via upload

--- parcial3/menu.php
+++ parcial3/menu.php
@@ -18,7 +18,7 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Opciones
+            Alumnos
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Acción</a></li>
