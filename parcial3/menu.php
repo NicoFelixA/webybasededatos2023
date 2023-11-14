af143767fb1df60b4cd9f76a709b53280e611327
NicoFelixA <124410869+NicoFelixA@users.noreply.github.com>
2023-11-13 18:38:11 -0700

    Add files via upload

--- parcial3/menu.php
+++ parcial3/menu.php
@@ -11,7 +11,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Inicio</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
